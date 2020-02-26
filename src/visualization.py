c9be32be6ca7cf117e9d9e5e368b59bacacbbadb
Eric Denovellis <edeno@bu.edu>
2020-02-25 16:45:28 -0800

    Add movie code

--- src/visualization.py
+++ src/visualization.py
@@ -1,6 +1,68 @@
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from matplotlib.collections import LineCollection
+from matplotlib.colorbar import ColorbarBase, make_axes
+
+
+def make_movie(position, map_position, position_info, frame_rate=500,
+               movie_name="video_name.mp4"):
+    # Set up formatting for the movie files
+    Writer = animation.writers["ffmpeg"]
+    writer = Writer(fps=frame_rate, metadata=dict(artist="Me"), bitrate=1800)
+
+    fig, ax = plt.subplots(1, 1, figsize=(7, 7))
+    ax.set_facecolor('black')
+    position_2d = position_info.loc[:, ["x_position", "y_position"]]
+    ax.plot(
+        position_2d.values[:, 0],
+        position_2d.values[:, 1],
+        color="lightgrey",
+        alpha=0.4,
+        zorder=1,
+    )
+
+    ax.set_xlim(position_info.x_position.min() - 1,
+                position_info.x_position.max() + 1)
+    ax.set_ylim(position_info.y_position.min() + 1,
+                position_info.y_position.max() + 1)
+    ax.set_xlabel("x-position")
+    ax.set_ylabel("y-position")
+
+    position = np.asarray(position)
+    position_dot = plt.scatter(
+        [], [], s=80, zorder=102, color="b", label="Actual"
+    )
+    (position_line,) = plt.plot([], [], "b-", linewidth=3)
+    sns.despine()
+    map_dot = plt.scatter([], [], s=80, zorder=102, color="r", label="Decoded")
+    (map_line,) = plt.plot([], [], "r-", linewidth=3)
+    ax.legend(fontsize=9, loc='upper right')
+    n_frames = map_position.shape[0]
+
+    def _update_plot(time_ind):
+        start_ind = max(0, time_ind - 5)
+        time_slice = slice(start_ind, time_ind)
+
+        position_dot.set_offsets(position[time_ind])
+        position_line.set_data(
+            position[time_slice, 0], position[time_slice, 1])
+
+        map_dot.set_offsets(map_position[time_ind])
+        map_line.set_data(
+            map_position[time_slice, 0], map_position[time_slice, 1])
+
+        return position_dot, map_dot
+
+    movie = animation.FuncAnimation(
+        fig, _update_plot, frames=n_frames, interval=1000 / frame_rate,
+        blit=True
+    )
+    if movie_name is not None:
+        movie.save(movie_name, writer=writer)
+
+    return fig, movie
 
 
 def plot_classifier_time_slice(
